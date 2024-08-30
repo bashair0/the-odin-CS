b43b07136b66d6a7e167bf378130771c673e909b
bashair0 <bisho5.ba@gmail.com>
2024-08-30 10:57:13 +0800

    Add fibRec function which prints out an array of fibonacci numbers up until n using recursion

--- fibonacci.js
+++ fibonacci.js
@@ -12,4 +12,14 @@ function fibs (n) {
   return array
 }
 
-console.log(fibs(5))
+function fibRec (n) {
+  let array = [0, 1]
+  if (n == 0) return 0
+  if (n == 1) return 1
+  for (let i = 2; i < n; i++) {
+    array[i] = array[i - 2] + array[i - 1]
+  }
+  return array
+}
+
+console.log(fibRec(5))
